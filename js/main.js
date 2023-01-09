ced596265db2371825b688be9dde8881c6f50319
Gamer Shiba Inu <54320883+GamerShibaInu@users.noreply.github.com>
2023-01-09 12:56:35 +0100

    monke

--- js/main.js
+++ js/main.js
@@ -1,36 +1,54 @@
-// add player names
+// first time visit
+const newVisit = localStorage.getItem("newVisit");
+const visitedPlrOne = window.localStorage.getItem("plrOne");
+const visitedPlrTwo = window.localStorage.getItem("plrTwo");
+if (newVisit == undefined) { // new visitor
+   localStorage.setItem("newVisit", 1)
+   alert("Welcome!");
+
+   // add player names
 const plrOneName = prompt("please enter player 1 name","Player 1")
-console.log(plrOneName)
 
 const addPlrName1 = document.querySelector(".plr-one")
 addPlrName1.textContent = plrOneName
 
-
 const plrTwoName = prompt("please enter player 2 name","Player 2")
-console.log(plrTwoName)
 
 const addPlrName2 = document.querySelector(".plr-two")
 addPlrName2.textContent = plrTwoName
 
 // save player names
-window.localStorage.setItem('PlrOne', plrOneName);
-window.localStorage.setItem('PlrTwo', plrTwoName);
+window.localStorage.setItem('plrOne', plrOneName);
+window.localStorage.setItem('plrTwo', plrTwoName);
+
+} else { // already visited
+ vPlrOne = document.querySelector(".plr-one")
+ vPlrOne.textContent = visitedPlrOne
+
+ vPlrTwo = document.querySelector(".plr-two")
+ vPlrTwo.textContent = visitedPlrTwo
+    alert("Welcome back!");
+}
 
 //turns
+
 const squares = document.querySelectorAll('.square');
 let plrTurn = "x"
+let whosTurn = document.querySelector(".turn-text")
+whosTurn.textContent = "its " + visitedPlrOne + "'s turn!"
 for (let index = 0; index < squares.length; index++) {
   const square = squares[index];
   square.addEventListener("click", function() {
     if (square.textContent === "") {
       square.textContent = plrTurn;
       if (plrTurn === "x") {
+        whosTurn.textContent = "its " + visitedPlrTwo + "'s turn!"
         plrTurn = "o";
       } else {
+        whosTurn.textContent = "its " + visitedPlrOne + "'s turn!"
         plrTurn = "x";
       }
     }
   });
 }
-
 
