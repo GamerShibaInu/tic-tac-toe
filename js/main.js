d73f45f857085818275d32f2728fde520f1ad141
Gamer Shiba Inu <54320883+GamerShibaInu@users.noreply.github.com>
2023-01-19 15:58:48 +0100

    Mobile responsive
    added @media to change if the screen is small to make it mobile friendly

--- js/main.js
+++ js/main.js
@@ -3,31 +3,31 @@ const newVisit = localStorage.getItem("newVisit");
 const visitedPlrOne = window.localStorage.getItem("plrOne");
 let plrOneName
 let plrTwoName
-function askPlr1(){
-  plrOneName = prompt("please enter player 1 name","Player 1");
+function askPlr1() {
+  plrOneName = prompt("please enter player 1 name", "Player 1");
   return
 }
-function askPlr2(){
-  plrTwoName = prompt("please enter player 2 name","Player 2");
-return
+function askPlr2() {
+  plrTwoName = prompt("please enter player 2 name", "Player 2");
+  return
 }
 const visitedPlrTwo = window.localStorage.getItem("plrTwo");
-if (newVisit == undefined) { 
+if (newVisit == undefined) {
   // new visitor
-   localStorage.setItem("newVisit", 1);
-   alert("Welcome!");
+  localStorage.setItem("newVisit", 1);
+  alert("Welcome!");
   askPlr1();
   askPlr2();
-   // add player names
-const addPlrName1 = document.querySelector(".plr-one");
-addPlrName1.textContent = plrOneName;
+  // add player names
+  const addPlrName1 = document.querySelector(".plr-one");
+  addPlrName1.textContent = plrOneName;
 
-const addPlrName2 = document.querySelector(".plr-two");
-addPlrName2.textContent = plrTwoName;
+  const addPlrName2 = document.querySelector(".plr-two");
+  addPlrName2.textContent = plrTwoName;
 
-// save player names
-localStorage.setItem("plrOne", plrOneName);
-localStorage.setItem("plrTwo", plrTwoName);
+  // save player names
+  localStorage.setItem("plrOne", plrOneName);
+  localStorage.setItem("plrTwo", plrTwoName);
 } else { // already visited
   vPlrOne = document.querySelector(".plr-one");
   vPlrOne.textContent = visitedPlrOne;
@@ -49,7 +49,7 @@ plrOneName = visitedPlrOne
 whosTurn.textContent = "its " + plrOneName + "'s turn!";
 for (let index = 0; index < squares.length; index++) {
   const square = squares[index];
-  square.addEventListener("click", function() {
+  square.addEventListener("click", function () {
     if (square.textContent === "") {
       square.textContent = plrTurn;
       if (plrTurn === "x") {
@@ -78,7 +78,7 @@ const box8 = document.querySelector(".box8");
 const box9 = document.querySelector(".box9");
 
 // clear boxes after win
-function clearBoxes(){
+function clearBoxes() {
   box1.textContent = ""
   box2.textContent = ""
   box3.textContent = ""
@@ -89,10 +89,10 @@ function clearBoxes(){
   box8.textContent = ""
   box9.textContent = ""
 
-  
+
 }
-  //winning player symbol (X)
-function xWon(){
+//winning player symbol (X)
+function xWon() {
   box1.textContent = "x"
   box2.textContent = "x"
   box3.textContent = "x"
@@ -102,41 +102,41 @@ function xWon(){
   box7.textContent = "x"
   box8.textContent = "x"
   box9.textContent = "x"
-  }
+}
 
-  //winning player symbol (O)
-  function oWon(){
-    box1.textContent = "o"
-    box2.textContent = "o"
-    box3.textContent = "o"
-    box4.textContent = "o"
-    box5.textContent = "o"
-    box6.textContent = "o"
-    box7.textContent = "o"
-    box8.textContent = "o"
-    box9.textContent = "o"
-    }
+//winning player symbol (O)
+function oWon() {
+  box1.textContent = "o"
+  box2.textContent = "o"
+  box3.textContent = "o"
+  box4.textContent = "o"
+  box5.textContent = "o"
+  box6.textContent = "o"
+  box7.textContent = "o"
+  box8.textContent = "o"
+  box9.textContent = "o"
+}
 
 const resetButton = document.querySelector(".resetBtn");
 resetButton.addEventListener("click", clearBoxes)
-    // X win flash
-  function xWinner(){
-    xWon()
-   let blinkTimer = setTimeout(clearBoxes, 1001)
-   xWon()
-   blinkTimer = setTimeout(clearBoxes, 1002)
-  }
+// X win flash
+function xWinner() {
+  xWon()
+  let blinkTimer = setTimeout(clearBoxes, 1001)
+  xWon()
+  blinkTimer = setTimeout(clearBoxes, 1002)
+}
 
-      // O win flash
-      function oWinner(){
-        oWon()
-       let blinkTimer = setTimeout(clearBoxes, 1002)
-       oWon()
-       blinkTimer = setTimeout(clearBoxes, 1002)
-      }
-    
+// O win flash
+function oWinner() {
+  oWon()
+  let blinkTimer = setTimeout(clearBoxes, 1002)
+  oWon()
+  blinkTimer = setTimeout(clearBoxes, 1002)
+}
 
-  // leaderboard variables 
+
+// leaderboard variables 
 const xLeaderboard = document.querySelector(".plr-one-score");
 const oLeaderboard = document.querySelector(".plr-two-score");
 let xWins = 0
@@ -144,85 +144,85 @@ let oWins = 0
 
 
 // leaderstats functions
-  function addWinX(){
-    xWins = xWins + 1
-    xLeaderboard.textContent = plrOneName + " has " + xWins + " wins";
-  }
+function addWinX() {
+  xWins = xWins + 1
+  xLeaderboard.textContent = plrOneName + " has " + xWins + " wins";
+}
 
-  function addWinO(){
-    oWins = oWins + 1
-    oLeaderboard.textContent = plrTwoName + " has " + oWins + " wins";
-  }
+function addWinO() {
+  oWins = oWins + 1
+  oLeaderboard.textContent = plrTwoName + " has " + oWins + " wins";
+}
 
 // win conditions X
-function winCheck(){
-  if (box1.textContent =="x" && box2.textContent =="x" && box3.textContent =="x") {
+function winCheck() {
+  if (box1.textContent == "x" && box2.textContent == "x" && box3.textContent == "x") {
     whosTurn.textContent = plrOneName + " won!"
     addWinX()
     xWinner()
-} else if (box4.textContent =="x" && box5.textContent =="x" && box6.textContent =="x"){
-  addWinX()
-  xWinner()
-  whosTurn.textContent = plrOneName + " won!"
-} else if (box7.textContent =="x" && box8.textContent =="x" && box9.textContent =="x"){
-  addWinX()
-  xWinner()
-  whosTurn.textContent = plrOneName + " won!"
-} else if (box1.textContent =="x" && box4.textContent =="x" && box7.textContent =="x"){
-  addWinX()
-  xWinner()
-  whosTurn.textContent = plrOneName + " won!"
-}
-else if (box2.textContent =="x" && box5.textContent =="x" && box8.textContent =="x"){
-  addWinX()
-  xWinner()
-  whosTurn.textContent = plrOneName + " won!"
-} else if (box3.textContent =="x" && box6.textContent =="x" && box9.textContent =="x"){
-  addWinX()
-  xWinner()
-  whosTurn.textContent = plrOneName + " won!"
-} else if (box1.textContent =="x" && box5.textContent =="x" && box9.textContent =="x"){
-  addWinX()
-  xWinner()
-  whosTurn.textContent = plrOneName + " won!"
-} else if (box3.textContent =="x" && box5.textContent =="x" && box7.textContent =="x"){
-  addWinX()
-  xWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-  // o win conditions
-}else if (box1.textContent =="o" && box2.textContent =="o" && box3.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-}else if (box4.textContent =="o" && box5.textContent =="o" && box6.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-}else if (box7.textContent =="o" && box8.textContent =="o" && box9.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-}else if (box1.textContent =="o" && box4.textContent =="o" && box7.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-}else if (box2.textContent =="o" && box5.textContent =="o" && box8.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-}else if (box3.textContent =="o" && box6.textContent =="o" && box9.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-}else if (box1.textContent =="o" && box5.textContent =="o" && box9.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
-}else if (box3.textContent =="o" && box5.textContent =="o" && box7.textContent =="o"){
-  addWinO()
-  oWinner()
-  whosTurn.textContent = plrTwoName + " won!"
+  } else if (box4.textContent == "x" && box5.textContent == "x" && box6.textContent == "x") {
+    addWinX()
+    xWinner()
+    whosTurn.textContent = plrOneName + " won!"
+  } else if (box7.textContent == "x" && box8.textContent == "x" && box9.textContent == "x") {
+    addWinX()
+    xWinner()
+    whosTurn.textContent = plrOneName + " won!"
+  } else if (box1.textContent == "x" && box4.textContent == "x" && box7.textContent == "x") {
+    addWinX()
+    xWinner()
+    whosTurn.textContent = plrOneName + " won!"
+  }
+  else if (box2.textContent == "x" && box5.textContent == "x" && box8.textContent == "x") {
+    addWinX()
+    xWinner()
+    whosTurn.textContent = plrOneName + " won!"
+  } else if (box3.textContent == "x" && box6.textContent == "x" && box9.textContent == "x") {
+    addWinX()
+    xWinner()
+    whosTurn.textContent = plrOneName + " won!"
+  } else if (box1.textContent == "x" && box5.textContent == "x" && box9.textContent == "x") {
+    addWinX()
+    xWinner()
+    whosTurn.textContent = plrOneName + " won!"
+  } else if (box3.textContent == "x" && box5.textContent == "x" && box7.textContent == "x") {
+    addWinX()
+    xWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+    // o win conditions
+  } else if (box1.textContent == "o" && box2.textContent == "o" && box3.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+  } else if (box4.textContent == "o" && box5.textContent == "o" && box6.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+  } else if (box7.textContent == "o" && box8.textContent == "o" && box9.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+  } else if (box1.textContent == "o" && box4.textContent == "o" && box7.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+  } else if (box2.textContent == "o" && box5.textContent == "o" && box8.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+  } else if (box3.textContent == "o" && box6.textContent == "o" && box9.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+  } else if (box1.textContent == "o" && box5.textContent == "o" && box9.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
+  } else if (box3.textContent == "o" && box5.textContent == "o" && box7.textContent == "o") {
+    addWinO()
+    oWinner()
+    whosTurn.textContent = plrTwoName + " won!"
 
-}
+  }
 }
 console.log("JavaSCript loaded successfully.")
